d69a661d7e9042b98886bd1d9df65f945d4237d3
Adam Morris <adam@kernelpanicstudios.com>
2022-04-29 21:17:24 -0500

    Removed Diesel

--- migrations/2021-06-12-033739_create_tables/down.sql
+++
@@ -1,4 +0,0 @@
-DROP TABLE "storehouse";
-DROP TABLE "group";
-DROP TABLE "user";
-DROP TABLE "user_group";
--- migrations/2021-06-12-040303_create_type_table/down.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "storehouse" DROP CONSTRAINT storehouse_type_fkey;
-ALTER TABLE "storehouse" DROP COLUMN "type";
-DROP TABLE "type";
